2237450998e85ab9893919ea6e23bcb6d82ea5c3
Amro Mukhtar <amromuktar@gmail.com>
2021-04-08 13:14:49 +0400

    Fixed Perfromance over 98

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({ secret: 'capo oee system', saveUninitialized: false, resave: f
 
 app.use(router);
 
-mongoose.connect('mongodb://localhost/database');
+mongoose.connect('mongodb://localhost/section1');
 
 var db = mongoose.connection;
 db.on('error', () => {
